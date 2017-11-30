347da3b9c273d5248f32f9b6ed4f682ba5c0705e
John Wojciechowski <jwojo13@gmail.com>
2017-11-30 22:39:03 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  attr_accessor :name, :artist, :genre
+
+    def initialize(name,artist,genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@count += 1
+      @@genres << genre
+      @@artists << artist
+    end
+
+    def self.count
+      @@count
+    end
+
+    def self.genres
+      @@genres.uniq
+    end
+
+    def self.artists
+      @@artists.uniq
+    end
+
+    def self.genre_count
+      genre_list = {}
+      @@genres.each do |target|
+        if genre_list["#{target}"] == nil
+          genre_list["#{target}"] = 1
+
+        else
+          genre_list.each do |meow,number|
+            genre_list[:meow] = number
+      end
+      end
+    end
+    genre_list
+  end
+
+end
